3d36c815170e8b89a13c6ad6ecc4fb9938097e2c
Lukas Reithmeier <lukas.reithmeier@fh-hagenberg.at>
2020-04-19 19:51:26 +0200

    added sun rgbd config and dataset

--- samples/sun/sunrgbd.py
+++ samples/sun/sunrgbd.py
@@ -1,31 +1,28 @@
 """
 Mask R-CNN
-Configurations and data loading code for the synthetic Shapes dataset.
-This is a duplicate of the code in the noteobook train_shapes.ipynb for easy
-import into other notebooks, such as inspect_model.ipynb.
-
-Copyright (c) 2017 Matterport, Inc.
-Licensed under the MIT License (see LICENSE for details)
-Written by Waleed Abdulla
+Configurations and data loading code for the sun rgbd dataset.
+uses rgbd data
+@see https://github.com/matterport/Mask_RCNN/wiki#training-with-rgb-d-or-grayscale-images
 """
 
 import os
 import sys
-import math
-import random
-import numpy as np
 import cv2
+import numpy as np
+import skimage.draw
 
 # Root directory of the project
 ROOT_DIR = os.path.abspath("../../")
 
 # Import Mask RCNN
 sys.path.append(ROOT_DIR)  # To find local version of the library
-from mrcnn.config import Config
+
 from mrcnn import utils
 
+from samples.sun.sunrgb import SunRGBConfig
 
-class SunRGBDConfig(Config):
+
+class SunRGBDConfig(SunRGBConfig):
     """Configuration for training on the sun rgbd dataset.
     Derives from the base Config class and overrides values specific
     to the sun rgbd dataset.
@@ -33,171 +30,138 @@ class SunRGBDConfig(Config):
     # Give the configuration a recognizable name
     NAME = "sunrgbd"
 
-    # Train on 1 GPU and 8 images per GPU. We can put multiple images on each
-    # GPU because the images are small. Batch size is 8 (GPUs * images/GPU).
-    GPU_COUNT = 1
-    IMAGES_PER_GPU = 1
-
-    # Number of classes (including background)
-    NUM_CLASSES = 1 + 13  # background + 13 classes
-
-    # Use small images for faster training. Set the limits of the small side
-    # the large side, and that determines the image shape.
-    IMAGE_MIN_DIM = 512
-    IMAGE_MAX_DIM = 512
-
-    # Use smaller anchors because our image and objects are small
-    # RPN_ANCHOR_SCALES = (8, 16, 32, 64, 128)  # anchor side in pixels
-
-    # Reduce training ROIs per image because the images are small and have
-    # few objects. Aim to allow ROI sampling to pick 33% positive ROIs.
-    TRAIN_ROIS_PER_IMAGE = 32
-
-    # Use a small epoch since the data is simple
-    STEPS_PER_EPOCH = 100
-
-    # use small validation steps since the epoch is small
-    # VALIDATION_STEPS = 5
-
-    # Skip detections with < 90% confidence
-    DETECTION_MIN_CONFIDENCE = 0.9
+    # 3 color channels +  1 depth channel
+    IMAGE_CHANNEL_COUNT = 4
+    MEAN_PIXEL = 4
 
 
-class SunRGBDDataset(utils.Dataset):
+class SunRGBDataset(utils.Dataset):
     """Generates the sun rgbd dataset.
     """
 
     def load_sun_rgbd(self, dataset_dir, subset):
         """Load a subset of the sun rgbd dataset.
+        Only use the rgb images
         dataset_dir: Root directory of the dataset.
         subset: Subset to load: train or val
         """
         # Add classes. We have only one class to add.
-        self.add_class("sun", 1, "bed")
-        self.add_class("sun", 2, "books")
-        self.add_class("sun", 3, "ceiling")
-        self.add_class("sun", 4, "chair")
-        self.add_class("sun", 5, "floor")
-        self.add_class("sun", 6, "furniture")
-        self.add_class("sun", 7, "objects")
-        self.add_class("sun", 8, "picture")
-        self.add_class("sun", 9, "sofa")
-        self.add_class("sun", 10, "table")
-        self.add_class("sun", 11, "tv")
-        self.add_class("sun", 12, "wall")
-        self.add_class("sun", 13, "window")
+        self.add_class("sunrgbd", 1, "bed")
+        self.add_class("sunrgbd", 2, "books")
+        self.add_class("sunrgbd", 3, "ceiling")
+        self.add_class("sunrgbd", 4, "chair")
+        self.add_class("sunrgbd", 5, "floor")
+        self.add_class("sunrgbd", 6, "furniture")
+        self.add_class("sunrgbd", 7, "objects")
+        self.add_class("sunrgbd", 8, "picture")
+        self.add_class("sunrgbd", 9, "sofa")
+        self.add_class("sunrgbd", 10, "table")
+        self.add_class("sunrgbd", 11, "tv")
+        self.add_class("sunrgbd", 12, "wall")
+        self.add_class("sunrgbd", 13, "window")
 
         # Train or validation dataset?
-        assert subset in ["train", "val"]
-        dataset_dir = os.path.join(dataset_dir, subset)
+        assert subset in ["train13", "test13", "split/test13", "split/val13"]
+        dataset_file = os.path.join(dataset_dir, subset + ".txt")
 
         # Load annotations
-        # VGG Image Annotator saves each image in the form:
-        # { 'filename': '28503151_5b5b7ec140_b.jpg',
-        #   'regions': {
-        #       '0': {
-        #           'region_attributes': {},
-        #           'shape_attributes': {
-        #               'all_points_x': [...],
-        #               'all_points_y': [...],
-        #               'name': 'polygon'}},
-        #       ... more regions ...
-        #   },
-        #   'size': 100202
-        # }
-        # We mostly care about the x and y coordinates of each region
-        annotations = json.load(open(os.path.join(dataset_dir, "via_region_data.json")))
-        annotations = list(annotations.values())  # don't need the dict keys
-
-        # The VIA tool saves images in the JSON even if they don't have any
-        # annotations. Skip unannotated images.
-        annotations = [a for a in annotations if a['regions']]
+        f = open(dataset_file, "r")
+        annotations = list(f)
+        f.close()
 
         # Add images
         for a in annotations:
-            # Get the x, y coordinaets of points of the polygons that make up
-            # the outline of each object instance. There are stores in the
-            # shape_attributes (see json format above)
-            polygons = [r['shape_attributes'] for r in a['regions'].values()]
-            objects = [s['region_attributes'] for s in a['regions'].values()]
-            class_ids = [int(n['class']) for n in objects]
-            # load_mask() needs the image size to convert polygons to masks.
-            # Unfortunately, VIA doesn't include it in JSON, so we must read
-            # the image. This is only managable since the dataset is tiny.
-            image_path = os.path.join(dataset_dir, a['filename'])
-            image = skimage.io.imread(image_path)
-            height, width = image.shape[:2]
+            files = a.split(" ")
+            rgb_image = files[0]
+            dpt_image = files[1]
+            lbl_image = files[2]
+
+            rgb_image_path = os.path.join(dataset_dir, rgb_image)
+            dpt_image_path = os.path.join(dataset_dir, dpt_image)
+            lbl_image_path = os.path.join(dataset_dir, lbl_image)
+            dpt_image_path = dpt_image_path.strip()
+            rgb_image_path = rgb_image_path.strip()
+            lbl_image_path = lbl_image_path.strip()
 
             self.add_image(
-                "sun",
-                image_id=a['filename'],  # use file name as a unique image id
-                path=image_path,
-                width=width, height=height,
-                polygons=polygons,
-                class_ids=class_ids)
-
-    def load_image(self, image_id):
-        """Generate an image from the specs of the given image ID.
-        Typically this function loads the image from a file, but
-        in this case it generates the image on the fly from the
-        specs in image_info.
-        """
-        info = self.image_info[image_id]
-        bg_color = np.array(info['bg_color']).reshape([1, 1, 3])
-        image = np.ones([info['height'], info['width'], 3], dtype=np.uint8)
-        image = image * bg_color.astype(np.uint8)
-        for shape, color, dims in info['shapes']:
-            image = self.draw_shape(image, shape, dims, color)
-        return image
+                "sunrgbd",
+                image_id=rgb_image,  # use file name as a unique image id
+                path=rgb_image_path,
+                dpt_image_path=dpt_image_path,
+                lbl_image_path=lbl_image_path)
 
     def image_reference(self, image_id):
-        """Return the shapes data of the image."""
+        """Return the path of the image."""
         info = self.image_info[image_id]
-        if info["source"] == "shapes":
-            return info["shapes"]
+        if info["source"] == "sunrgbd":
+            return info["path"]
         else:
-            super(self.__class__).image_reference(self, image_id)
+            super(self.__class__, self).image_reference(image_id)
 
     def load_mask(self, image_id):
-        """Generate instance masks for shapes of the given image ID.
+        """Generate instance masks for an image.
+       Returns:
+        masks: A bool array of shape [height, width, instance count] with
+            one mask per instance.
+        class_ids: a 1D array of class IDs of the instance masks.
         """
-        info = self.image_info[image_id]
-        shapes = info['shapes']
-        count = len(shapes)
-        mask = np.zeros([info['height'], info['width'], count], dtype=np.uint8)
-        for i, (shape, _, dims) in enumerate(info['shapes']):
-            mask[:, :, i:i + 1] = self.draw_shape(mask[:, :, i:i + 1].copy(),
-                                                  shape, dims, 1)
-        # Handle occlusions
-        occlusion = np.logical_not(mask[:, :, -1]).astype(np.uint8)
-        for i in range(count - 2, -1, -1):
-            mask[:, :, i] = mask[:, :, i] * occlusion
-            occlusion = np.logical_and(
-                occlusion, np.logical_not(mask[:, :, i]))
-        # Map class names to class IDs.
-        class_ids = np.array([self.class_names.index(s[0]) for s in shapes])
-        return mask, class_ids.astype(np.int32)
+        # If not a sun dataset image, delegate to parent class.
+        image_info = self.image_info[image_id]
+        if image_info["source"] != "sunrgbd":
+            return super(self.__class__, self).load_mask(image_id)
 
-    def random_image(self, height, width):
-        """Creates random specifications of an image with multiple shapes.
-        Returns the background color of the image and a list of shape
-        specifications that can be used to draw the image.
+        # Convert polygons to a bitmap mask of shape
+        # [height, width, instance_count]
+        info = self.image_info[image_id]
+        lbl_image_path = info["lbl_image_path"]
+        print("\"" + lbl_image_path + "\"")
+        lbl_image = cv2.imread(lbl_image_path, cv2.IMREAD_UNCHANGED)
+
+        height, width = lbl_image.shape[:2]
+
+        mask_found = []
+        class_ids = []
+
+        # 13 different classes
+        for cls in range(1, 13):
+            class_mask = (lbl_image == cls).astype(np.uint8)
+
+            # no instances of cls is present
+            if cv2.countNonZero(class_mask) == 0:
+                continue
+
+            class_contours, _ = cv2.findContours(class_mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+            print("class" + str(cls))
+            for i in range(0, len(class_contours)):
+                contour_mask = np.zeros([height, width], dtype=np.uint8)
+                cv2.drawContours(contour_mask, class_contours, i, cls, cv2.FILLED)
+                mask_found.append(contour_mask)
+                class_ids.append(cls)
+
+        # contours found per instance differs from contours found in the label image
+        # since the label image consists of numbers of 0-13 in a range of 0-255
+        # therefore openCV does not find contours that well
+        mask = np.zeros([height, width, len(mask_found)], dtype=np.uint8)
+        for i in range(0, len(mask_found)):
+            mask[:, :, i] = mask_found[i]
+
+        cv2.waitKey(0)
+        return mask, np.array(class_ids)
+
+    def load_image(self, image_id):
+        """Load the specified image and return a [H,W,4] Numpy array.
+        an image consists of 3 color channels and 1 depth channel
         """
-        # Pick random background color
-        bg_color = np.array([random.randint(0, 255) for _ in range(3)])
-        # Generate a few random shapes and record their
-        # bounding boxes
-        shapes = []
-        boxes = []
-        N = random.randint(1, 4)
-        for _ in range(N):
-            shape, color, dims = self.random_shape(height, width)
-            shapes.append((shape, color, dims))
-            x, y, s = dims
-            boxes.append([y - s, x - s, y + s, x + s])
-        # Apply non-max suppression wit 0.3 threshold to avoid
-        # shapes covering each other
-        keep_ixs = utils.non_max_suppression(
-            np.array(boxes), np.arange(N), 0.3)
-        shapes = [s for i, s in enumerate(shapes) if i in keep_ixs]
-        return bg_color, shapes
+        # Load image
+        rgb_image = super().load_image(image_id)
+        dpt_image = skimage.io.imread(self.image_info[image_id]['dpt_image_path'])
+        width = rgb_image.shape[0]
+        height = rgb_image.shape[1]
+        image = np.zeros([width, height, 4], np.uint8)
+
+        image[:, :, 0] = rgb_image[:, :, 0]
+        image[:, :, 1] = rgb_image[:, :, 1]
+        image[:, :, 2] = rgb_image[:, :, 2]
+        image[:, :, 3] = (dpt_image / 65535 * 255).astype(np.uint8)  # normalized conversion from uint16 to uint8
+
+        return image
