78dff07bd369889d2fa74fc19978013fd8886dd7
Lukas Reithmeier <Lukas.Reithmeier@fh-hagenberg.at>
2020-04-21 10:32:41 +0200

    added functionality to realsense_reader

--- samples/elevator/realsense_reader.py
+++ samples/elevator/realsense_reader.py
@@ -1,35 +1,121 @@
-# First import the library
-import pyrealsense2 as rs
-# Import Numpy for easy array manipulation
-import numpy as np
-# Import OpenCV for easy image rendering
+import argparse
+import os
+
 import cv2
-
-print("version:")
-print(rs)
-print(cv2.getVersionString())
-print(np.__version__)
-print("---")
-
-pipe = rs.pipeline()
-profile = pipe.start()
-try:
-    for i in range(0, 100):
-        frames = pipe.wait_for_frames()
-        for f in frames:
-            print(f.profile)
-finally:
-    pipe.stop()
-
-print("---")
+import numpy as np
+import pyrealsense2 as rs
 
 
-class RealSenseReader:
-    def __init__(self, file):
-        self.file = file
+def strip(file):
+    """
+    removes args.directory from file
+    :return: file without args.directory
+    """
+    return file.replace(args.directory, '.')
 
-    def read_frame(self):
-        rgb_frame = []
-        dpt_frame = []
-        intrinsics = []
-        return rgb_frame, dpt_frame, intrinsics
+
+def write_intrinsics(intrinsics, file):
+    """
+    writes intrinsics in json format to a file
+    """
+    f = open(file, "w")
+    f.write("{\n")
+    f.write("\t\"width\":" + str(intrinsics.width) + ",\n")
+    f.write("\t\"height\":" + str(intrinsics.height) + ",\n")
+    f.write("\t\"ppx\":" + str(intrinsics.ppx) + ",\n")
+    f.write("\t\"ppy\":" + str(intrinsics.ppy) + ",\n")
+    f.write("\t\"fx\":" + str(intrinsics.fx) + ",\n")
+    f.write("\t\"fy\":" + str(intrinsics.fy) + ",\n")
+    f.write("\t\"model\":" + str(int(intrinsics.model)) + ",\n")
+    f.write("\t\"coeffs\":" + str(intrinsics.coeffs) + "\n")
+    f.write("}")
+    f.close()
+
+
+def process_frames(rgb_dir, dpt_dir, rgb_itc_dir, dpt_itc_dir):
+    """
+    reads color and depth frame
+    aligns the frames to the color frame
+    writes depth frame, color frame and intrinsics to files
+    :param rgb_dir: directory to save color frames to
+    :param dpt_dir: directory to save depth frames to
+    :param rgb_itc_dir: directory to save color intrinsics to
+    :param dpt_itc_dir: directory to save depth intrinsics to
+    """
+
+    prefix = args.input.split(".")[0]  # name of file without extension
+    index_file = open(args.directory + "/" + "all.txt", "a")
+
+    try:
+        config = rs.config()
+        rs.config.enable_device_from_file(config, args.input)
+        pipeline = rs.pipeline()
+        config.enable_stream(rs.stream.depth, rs.format.z16, 30)
+        config.enable_stream(rs.stream.color, rs.format.rgb8, 30)
+        cfg = pipeline.start(config)
+        align_to = rs.stream.color
+        align = rs.align(align_to)
+
+        rgb_profile = cfg.get_stream(rs.stream.color)
+        dpt_profile = cfg.get_stream(rs.stream.depth)
+        rgb_intrinsics = rgb_profile.as_video_stream_profile().get_intrinsics()
+        dpt_intrinsics = dpt_profile.as_video_stream_profile().get_intrinsics()
+
+        i = 0
+        while True:
+            print("Saving frame:", i)
+            frames = pipeline.wait_for_frames()
+            aligned_frames = align.process(frames)
+
+            bgr_frame = aligned_frames.get_color_frame()
+            dpt_frame = aligned_frames.get_depth_frame()
+
+            bgr_image = np.asanyarray(bgr_frame.get_data())
+            rgb_image = cv2.cvtColor(bgr_image, cv2.COLOR_RGB2BGR)
+            dpt_image = np.asanyarray(dpt_frame.get_data())
+
+            rgb_file = rgb_dir + "/" + prefix + "_" + str(i).zfill(6) + ".jpg"
+            dpt_file = dpt_dir + "/" + prefix + "_" + str(i).zfill(6) + ".png"
+            rgb_itc_file = rgb_itc_dir + "/" + prefix + "_" + str(i).zfill(6) + ".json"
+            dpt_itc_file = dpt_itc_dir + "/" + prefix + "_" + str(i).zfill(6) + ".json"
+            cv2.imwrite(rgb_file, rgb_image)
+            cv2.imwrite(dpt_file, dpt_image)
+            write_intrinsics(rgb_intrinsics, rgb_itc_file)
+            write_intrinsics(dpt_intrinsics, dpt_itc_file)
+            index_file.write(strip(rgb_file) + " " +
+                             strip(dpt_file) + " " +
+                             strip(rgb_itc_file) + " " +
+                             strip(dpt_itc_file) + "\n")
+            i += 1
+    finally:
+        index_file.close()
+        pass
+
+
+def main():
+    if not os.path.exists(args.directory):
+        os.mkdir(args.directory)
+    rgb_dir = args.directory + "/rgb"
+    dpt_dir = args.directory + "/depth"
+    rgb_itc_dir = args.directory + "/rgb_intrinsics"
+    dpt_itc_dir = args.directory + "/depth_intrinsics"
+
+    if not os.path.exists(rgb_dir):
+        os.mkdir(rgb_dir)
+    if not os.path.exists(dpt_dir):
+        os.mkdir(dpt_dir)
+    if not os.path.exists(rgb_itc_dir):
+        os.mkdir(rgb_itc_dir)
+    if not os.path.exists(dpt_itc_dir):
+        os.mkdir(dpt_itc_dir)
+
+    process_frames(rgb_dir, dpt_dir, rgb_itc_dir, dpt_itc_dir)
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-d", "--directory", type=str, help="Path to save the images", default="./out")
+    parser.add_argument("-i", "--input", type=str, help="Bag file to read", default="20181008_105503.bag")
+    args = parser.parse_args()
+
+    main()
